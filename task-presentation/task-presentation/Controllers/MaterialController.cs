bec452cc32a65459957c6f79efbd09c314f32849
Vinícius Monteiro <vinicius@iconax.com.br>
2024-07-17 22:16:15 -0300

    adição de página para gerenciamento dos materiais

---
+++ task-presentation/task-presentation/Controllers/MaterialController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using task_presentation.Models;
+using static task_presentation.Models.Material;
+
+namespace task_presentation.Controllers
+{
+    public class MaterialController : Controller
+    {
+        // GET: Material
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CarregarListaFundo() {
+            var comboMateriais = Material.BuscarFundos();
+            var info = new PaginaInfo() { TituloPagina = "Fundo", ComboMateriais = comboMateriais };
+
+            return View("ListaMateriais", info);
+        }
+
+        public ActionResult CarregarListaMoldura() {
+            var comboMateriais = Material.BuscarMolduras();
+            var info = new PaginaInfo() { TituloPagina = "Tipos de Moldura", ComboMateriais = comboMateriais };
+
+            return View("ListaMateriais", info);
+        }
+
+        public ActionResult CarregarListaImpressao() {
+            var comboMateriais = Material.BuscarImpressao();
+            var info = new PaginaInfo() { TituloPagina = "Tipos de Impressão", ComboMateriais = comboMateriais };
+
+            return View("ListaMateriais", info);
+        }
+
+        public ActionResult CarregarListaVidro() {
+            var comboMateriais = Material.BuscarVidro();
+            var info = new PaginaInfo() { TituloPagina = "Tipos de Vidro", ComboMateriais = comboMateriais };
+
+            return View("ListaMateriais", info);
+        }
+    }
+}
---
+++ task-presentation/task-presentation/Models/Material.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using task_presentation.Database;
+
+namespace task_presentation.Models {
+    public class Material {
+
+        public class MaterialCombo {
+            public int ID { get; set; }
+            public string DS_NOME { get; set; }
+        }
+        public class PaginaInfo {
+            public string TituloPagina { get; set; }
+            public List<MaterialCombo> ComboMateriais { get; set; }
+        }
+
+        public static List<MaterialCombo> BuscarFundos() {
+            using (var banco = new TASK_PRESENTATIONEntities()) {
+
+                var list = (from f in banco.TIPO_FUNDO
+                            select new MaterialCombo() { 
+                                ID = f.CD_FUNDO,
+                                DS_NOME = f.DS_FUNDO
+                            }).ToList();
+
+                return list;
+            }
+        }
+        public static List<MaterialCombo> BuscarMolduras() {
+            using (var banco = new TASK_PRESENTATIONEntities()) {
+
+                var list = (from f in banco.TIPO_MOLDURA
+                            select new MaterialCombo() {
+                                ID = f.CD_MOLDURA,
+                                DS_NOME = f.DS_MOLDURA
+                            }).ToList();
+
+                return list;
+            }
+        }
+        public static List<MaterialCombo> BuscarImpressao() {
+            using (var banco = new TASK_PRESENTATIONEntities()) {
+
+                var list = (from f in banco.TIPO_IMPRESSAO
+                            select new MaterialCombo() {
+                                ID = f.CD_IMPRESSAO,
+                                DS_NOME = f.DS_IMPRESSAO
+                            }).ToList();
+
+                return list;
+            }
+        }
+        public static List<MaterialCombo> BuscarVidro() {
+            using (var banco = new TASK_PRESENTATIONEntities()) {
+
+                var list = (from f in banco.TIPO_VIDRO
+                            select new MaterialCombo() {
+                                ID = f.CD_VIDRO,
+                                DS_NOME = f.DS_VIDRO
+                            }).ToList();
+
+                return list;
+            }
+        }
+    }
+}
